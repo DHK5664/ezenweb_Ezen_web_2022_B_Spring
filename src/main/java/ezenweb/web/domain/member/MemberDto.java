a73ec422b0780e25bed38ce0baad674630eb08e2
DHK5664 <sunsinger5664@gmail.com>
2023-04-13 15:12:33 +0900

    Day 71 oauth2 구글 인증토큰 결과를 이용한 memberDto 와 통하하기

--- src/main/java/ezenweb/web/domain/member/MemberDto.java
+++ src/main/java/ezenweb/web/domain/member/MemberDto.java
@@ -3,14 +3,19 @@ package ezenweb.web.domain.member;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 
-// 시큐리티 + 일반DTO
+// 시큐리티[UserDetails] + 소셜회원[OAuth2User]
 @Getter@Setter@ToString@AllArgsConstructor@NoArgsConstructor@Builder
-public class MemberDto implements UserDetails {
+public class MemberDto implements UserDetails , OAuth2User {
+
+
+
     private int mno; // 1. 회원번호
     private String memail; // 2. 회원아이디[ 이메일 ]
     private String mpassword; // 3. 회원비밀번호
@@ -18,6 +23,7 @@ public class MemberDto implements UserDetails {
     private String mphone; // 5. 회원전화번호
     private String mrole; // 6. 회원등급 [ 가입용 ]
     private Set<GrantedAuthority> 권한목록; // 7.[ 인증용 ]
+    private Map<String, Object> 소셜회원정보; // 8. oauth2 인증 회원정보
 
     // 추가
     private LocalDateTime cdate;
@@ -31,6 +37,7 @@ public class MemberDto implements UserDetails {
                 .build();
     }
 
+    // ------------- UserDetails -----------------
     @Override // 인증된 권한 반환
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.권한목록;
@@ -64,5 +71,15 @@ public class MemberDto implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+    // ------------------ OAuth2User ------------------ //
+    @Override // Oauth2 회원정보
+    public Map<String, Object> getAttributes() {
+        return this.소셜회원정보;
+    }
+
+    @Override // Oauth2 아이디
+    public String getName() {
+        return this.memail;
     }
 }
