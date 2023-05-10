683e83963170dd4e084bb9a01f2231e6ac9052f5
DHK5664 <sunsinger5664@gmail.com>
2023-05-10 10:35:45 +0900

    Day 89 연관관계 제약조건 설정

--- src/main/java/ezenweb/web/domain/product/ProductEntity.java
+++ src/main/java/ezenweb/web/domain/product/ProductEntity.java
@@ -3,11 +3,12 @@ package ezenweb.web.domain.product;
 import ezenweb.web.domain.BaseTime;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor @Builder
 @Entity @Table(name="product")
@@ -21,6 +22,10 @@ public class ProductEntity extends BaseTime{
     @ColumnDefault("0")@Column(nullable = false) private  byte pstate; // 제품상태 [ 0:판매중 , 1:판매중지 , 2:재고없음 ]
     @ColumnDefault("0")@Column(nullable = false) private int pstock; // 제품 재고/수량
     // 제품이미지 [1:다] 연관관계 [*추후]
+    // pk필드 선언시 // mappedBy="참조할필드명" // 제약조건 : pk객체가 삭제되면 fk객체에 제약조건
+    @OneToMany(mappedBy = "productEntity" , cascade = CascadeType.REMOVE)
+    @ToString.Exclude @Builder.Default
+    private List<ProductImgEntity> productImgEntityList = new ArrayList<>();
     // 구매내역 [1:다] 연관관계 [*추후]
 
     // 1. 출력용 [관리자가 보는 입장 - 관리자 페이지에서 출력용]
@@ -38,3 +43,13 @@ public class ProductEntity extends BaseTime{
     // 2. 출력용 [사용자가 보는 입장 - 메인 페이지에서 출력용]
     //public ProductDto toAdminDto(){}
 }
+
+/*
+    제약조건 : pk객체가 삭제되면 fk객체에 제약조건
+        cascade = CascadeType.ALL	: pk객체 삭제/수정시 fk객체 모두 삭제/수정
+        cascade = CascadeType.MERGE	: pk객체 수정시 fk객체도 모두 수정
+        cascade = CascadeType.REMOVE	: pk객체 삭제시 fk객체도 모두 삭제
+            vs @OnDelete(action = OnDeleteAction.CASCADE)
+
+        cascade = CascadeType.DETACH	: pk객체 삭제/수정의 변경사항 fk에 반영 안함
+*/
